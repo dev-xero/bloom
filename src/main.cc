6541b555066e11c623f9faa0d99477ebc3be52ac
= <ssh.xero@gmail.com>
2025-09-06 01:25:08 +0100

    chore: improve main
    Signed-off-by: = <ssh.xero@gmail.com>

--- src/main.cc
+++ src/main.cc
@@ -12,9 +12,10 @@ int main() {
 
     for (int j = 0; j < image_height; j++) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+
         for (int i = 0; i < image_width; i++) {
-            auto pixel_color = color(double(i) / (image_width)-1, double(j) / (image_width)-1, 0.0);
-            write_color(std::cout, pixel_color);
+            auto pixel = color(double(i) / (image_width - 1), 0.0, double(j) / (image_width - 1));
+            write_color(std::cout, pixel);
         }
     }
 
