298bc7f51a004bc75ce4af80920b54f2b6ebae1e
= <ssh.xero@gmail.com>
2025-09-07 21:27:09 +0100

    refactor: sphere class and define hittables
    Signed-off-by: = <ssh.xero@gmail.com>

--- include/sphere.h
+++ include/sphere.h
@@ -1,9 +1,19 @@
 #ifndef SPHERE_H
 #define SPHERE_H
 
+#include "hittable.h"
 #include "ray.h"
 #include "vec3.h"
 
-double hit_sphere(const point3 &center, double radius, const ray &r);
+class sphere : public hittable {
+  public:
+    sphere(const point3 &center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+
+    bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const override;
+
+  private:
+    const point3 &center;
+    double radius;
+};
 
 #endif
--- src/sphere.cc
+++ src/sphere.cc
@@ -1,18 +1,21 @@
 #include "sphere.h"
 
-// Using vector algebra and formula of a sphere, determine
-// whether a ray hits the sphere
+// Using vector algebra and formula of a sphere, determine whether a ray hits the sphere
+//
 // (C-P).(C-P) = r^2
 // given P = Q + td,
 // (-td + (C-Q)).(-td + (C-Q)) = r^2
-// applying vector algebra,
+//
+// using vector algebra:
 // t^2d.d - 2td.(C-Q) + (C-Q).(C-Q) - r^2 = 0
+//
 // this is a quadratic equation solvable through the discriminant formula
 // if D > 0, we have 2 real solutions
-double hit_sphere(const point3 &center, double radius, const ray &r) {
+bool sphere::hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const {
     // Optimization:
-    // recall that a vector dotted with itself is equal to the squared
+    // Recall that a vector dotted with itself is equal to the squared
     // length of that vector, secondly setting b = -2h, we get:
+    //
     // 2h + 2(root(h^2 - ac)) / 2a
     // h + root(h^2 - ac) / a
     // b = -2d.(C-Q), and b = -2h, hence:
@@ -21,15 +24,26 @@ double hit_sphere(const point3 &center, double radius, const ray &r) {
     auto a = r.direction().length_squared();
     auto h = dot(r.direction(), oc);
     auto c = oc.length_squared() - radius * radius;
-    auto discriminant = h * h - a * c;
 
+    auto discriminant = h * h - a * c;
     if (discriminant < 0) {
-        return -1.0;
-    } else {
-        // since the ray can hit the sphere at two points (entry and exit)
-        // we want the closer of the two given by:
-        // t1 = (-b - root(D)) / 2a
-        // after the optimization: we can use h instead
-        return (h - std::sqrt(discriminant)) / a;
+        return false;
     }
+
+    auto sqrtd = std::sqrt(discriminant);
+
+    // We need to find the nearest root within accepted range
+    auto root = (h - sqrtd) / a;
+    if (root <= ray_tmin || ray_tmax <= root) {
+        root = (h + sqrtd) / a;
+        if (root <= ray_tmin || ray_tmax <= root) {
+            return false;
+        }
+    }
+
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    rec.normal = (rec.p - center) / radius;
+
+    return true;
 }
