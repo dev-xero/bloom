6202b0908dd8cafe3df888cf8a772b3a6e73940d
= <ssh.xero@gmail.com>
2025-09-07 21:37:32 +0100

    feat: add front face normal calculation
    Signed-off-by: = <ssh.xero@gmail.com>

--- include/hittable.h
+++ include/hittable.h
@@ -8,6 +8,9 @@ class hit_record {
     point3 p;
     vec3 normal;
     double t;
+    bool front_face;
+
+    void set_face_normal(const ray &r, const vec3 &outward_normal);
 };
 
 class hittable {
---
+++ src/hittable.cc
@@ -0,0 +1,8 @@
+#include "hittable.h"
+
+void hit_record::set_face_normal(const ray &r, const vec3 &outward_normal) {
+    // Sets the hit record normal vector
+    // NOTE: the parameter `outward_normal` is assumed to have unit length
+    hit_record::front_face = dot(r.direction(), outward_normal) < 0;
+    hit_record::normal = front_face ? outward_normal : -outward_normal;
+}
