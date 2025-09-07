db75ee77cfb3f3944233fc4fb00dd5e610a8e376
= <ssh.xero@gmail.com>
2025-09-07 21:50:41 +0100

    feat: implement hittable lists
    Signed-off-by: = <ssh.xero@gmail.com>

---
+++ include/hittable_list.h
@@ -0,0 +1,25 @@
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include "hittable.h"
+
+#include <memory>
+#include <vector>
+
+using std::make_shared;
+using std::shared_ptr;
+
+class hittable_list : public hittable {
+  public:
+    std::vector<shared_ptr<hittable>> objects;
+
+    hittable_list() {}
+    hittable_list(shared_ptr<hittable> object) { add(object); }
+
+    void clear() { objects.clear(); }
+    void add(shared_ptr<hittable> object) { objects.push_back(object); }
+
+    bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const override;
+};
+
+#endif
