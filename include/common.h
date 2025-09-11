8a99f5b74a37fd2fa17fff52a2eeb0e404d92166
= <ssh.xero@gmail.com>
2025-09-11 02:22:51 +0100

    chore: add random number gen utilities
    Signed-off-by: = <ssh.xero@gmail.com>

--- include/common.h
+++ include/common.h
@@ -2,6 +2,7 @@
 #define COMMON_H
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
 
@@ -15,5 +16,15 @@ const double pi = 3.1415926535897932385;
 
 // Utility functions
 inline double degrees_to_radians(double degrees) { return degrees * pi / 180.0; }
+
+inline double random_double() {
+    // returns a random real in [0, 1)
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // returns a random real in [min, max]
+    return min + (max - min) * random_double();
+}
 
 #endif
