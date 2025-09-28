d2199411bd51fd3fa4152ce7c020fecd948f1e31
= <ssh.xero@gmail.com>
2025-09-28 01:00:54 +0100

    chore: fix spelling
    Signed-off-by: = <ssh.xero@gmail.com>

--- src/materials/lambertian.cc
+++ src/materials/lambertian.cc
@@ -1,11 +1,11 @@
 #include "lambertian.hpp"
 
 bool Lambertian::Scatter(const Ray &ray_in, const HitRecord &rec, Color &atten, Ray &scat) const {
-    auto scater_direction = rec.normal_ + RandomUnitVector();
+    auto scatter_direction = rec.normal_ + RandomUnitVector();
 
     // Catch degenerate scatter direction
-    if (scater_direction.NearZero()) {
-        scater_direction = rec.normal_;
+    if (scatter_direction.NearZero()) {
+        scatter_direction = rec.normal_;
     }
 
     atten = albedo_;
